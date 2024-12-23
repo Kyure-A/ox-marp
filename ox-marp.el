8b7d48d87f8b7ce09e8af3870337aec4b663658c
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-12-23 23:33:39 +0900

    chore: initialize emacs project

---
+++ ox-marp.el
@@ -0,0 +1,40 @@
+;;; ox-marp.el --- Org export engine for marp  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Kyure_A
+
+;; Author: Kyure_A <github.com/Kyure-A>
+;; Keywords: tools
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/Kyure-A/ox-marp
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Org export engine for marp
+
+;;; Code:
+
+(defgroup ox-marp ()
+  "Org export engine for marp"
+  :group 'tools
+  :prefix "ox-marp-"
+  :link '(url-link "https://github.com/Kyure-A/ox-marp"))
+
+(provide 'ox-marp)
+;;; ox-marp.el ends here
