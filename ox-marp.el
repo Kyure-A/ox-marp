987035e708bc5522dd2fab83a62cfebcab27c492
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-12-24 00:06:04 +0900

    feat: impl basic function by gpt-4o

--- ox-marp.el
+++ ox-marp.el
@@ -5,8 +5,8 @@
 ;; Author: Kyure_A <github.com/Kyure-A>
 ;; Keywords: tools
 
-;; Version: 0.0.0
-;; Package-Requires: ((emacs "24.1"))
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.1") (ox))
 ;; URL: https://github.com/Kyure-A/ox-marp
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
@@ -30,11 +30,83 @@
 
 ;;; Code:
 
-(defgroup ox-marp ()
+(require 'ox)
+
+(defgroup org-export-marp ()
   "Org export engine for marp"
   :group 'tools
-  :prefix "ox-marp-"
+  :prefix "org-marp-"
   :link '(url-link "https://github.com/Kyure-A/ox-marp"))
+
+(defconst org-marp-options-alist
+  '((:marp-theme "MARPTHEME" nil nil t)
+    (:marp-paginate "MARPPAGINATE" nil nil t)))
+
+(org-export-define-derived-backend 'marp 'md
+  :menu-entry
+  '(?M "Export to Marp"
+       ((?m "To file" org-marp-export-to-file)
+        (?p "To file and open" org-marp-export-to-file-and-open)))
+  :options-alist org-marp-options-alist
+  :translate-alist '((template . org-marp-template)
+                     (headline . org-marp-headline)
+                     (link . org-marp-link)
+                     (src-block . org-marp-src-block)
+                     (example-block . org-marp-example-block)))
+
+(defun org-marp-template (contents info)
+  "Marp 用のヘッダーと CONTENTS を結合して返す."
+  (let ((theme (plist-get info :marp-theme))
+        (paginate (plist-get info :marp-paginate)))
+    (concat
+     "---\n"
+     (when theme (format "theme: %s\n" theme))
+     (when paginate "paginate: true\n")
+     "---\n\n"
+     contents)))
+
+;; 見出しをスライド区切りに対応させる
+(defun org-marp-headline (headline contents info)
+  "HEADLINE を Marp 用スライド形式に変換"
+  (let ((title (org-export-data (org-element-property :title headline) info)))
+    (concat
+     (format "# %s\n" title)
+     "---\n"
+     contents)))
+
+;; リンクを Markdown の形式に変換
+(defun org-marp-link (link desc info)
+  "LINK を Markdown のリンク形式に変換"
+  (let ((url (org-element-property :raw-link link)))
+    (format "[%s](%s)" (or desc url) url)))
+
+;; ソースコードブロックを Marp のコードブロックに変換
+(defun org-marp-src-block (src-block contents info)
+  "SRC-BLOCK を Markdown のコードフェンス形式に変換"
+  (let ((lang (org-element-property :language src-block))
+        (value (org-element-property :value src-block)))
+    (format "```%s\n%s\n```\n" (or lang "") value)))
+
+;; 実例ブロックをそのままコードフェンスで囲む
+(defun org-marp-example-block (example-block contents info)
+  "EXAMPLE-BLOCK を Markdown のコードフェンス形式に変換"
+  (let ((value (org-element-property :value example-block)))
+    (format "```\n%s\n```\n" value)))
+
+;; エクスポート用の関数
+(defun org-marp-export-to-file (&optional async subtreep visible-only body-only ext-plist)
+  "現在の Org バッファを Marp 用 Markdown ファイルにエクスポート"
+  (interactive)
+  (let ((outfile (org-export-output-file-name ".md" subtreep)))
+    (org-export-to-file 'marp outfile
+      async subtreep visible-only body-only ext-plist)))
+
+(defun org-marp-export-to-file-and-open (&optional async subtreep visible-only body-only ext-plist)
+  "現在の Org バッファを Marp 用 Markdown ファイルにエクスポートし開く"
+  (interactive)
+  (let ((outfile (org-marp-export-to-file async subtreep visible-only body-only ext-plist)))
+    (when outfile
+      (browse-url-of-file outfile))))
 
 (provide 'ox-marp)
 ;;; ox-marp.el ends here
